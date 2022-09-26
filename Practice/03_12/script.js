1b312d40d20f3d4606de7c18d41d9b85911aeb04
Jade Yao <jadexy2014@gmail.com>
2022-09-26 23:45:26 +1300

    object constructor

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,32 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+class Clothing {
+    constructor (
+    // what I want to include
+    name,
+    color,
+    length,
+    season,
+    ){
+        //Define 
+        this. name = name;
+        this. color = color;
+        this. length = length;
+        this. season = season 
+    }
+    changeSeason(newSeason) {
+        this.season = newSeason 
+    }  
+}
+
+const coat = new Clothing(
+    "coat",
+    "green",
+    56,
+    "winter"
+);
+
+console.log("the season:", coat.season);
+coat.changeSeason("spring")
+console.log("the season:", coat.season);
